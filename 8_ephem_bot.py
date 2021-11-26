7de2f7018babf20afd1a61d8bb6449ff89bb9972
Shestakov Aleksandr <santellla@mail.ru>
2021-11-26 22:17:37 +0300

    changes in gitignore

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,7 +12,9 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
+import ephem
 import logging
+import keys_and_directories
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -21,13 +23,10 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     filename='bot.log')
 
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
+PROXY = {'proxy_url': keys_and_directories.PROXY_URL,
     'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
+      'username': keys_and_directories.PROXY_USERNAME, 
+      'password': keys_and_directories.PROXY_PASSWORD}}
 
 
 def greet_user(update, context):
@@ -39,11 +38,11 @@ def greet_user(update, context):
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text(ephem.Planet)
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(keys_and_directories.API_KEY, request_kwargs=PROXY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
